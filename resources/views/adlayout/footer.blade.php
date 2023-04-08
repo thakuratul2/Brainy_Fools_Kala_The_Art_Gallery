aa77f759a01a6b45f12eb58396bd275b89668435
thakuratul2 <pratapsinghatul111@gmail.com>
2023-04-08 11:17:07 +0530

    Admin Page Added

---
+++ resources/views/adlayout/footer.blade.php
@@ -0,0 +1,29 @@
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer">
+    <div class="copyright">
+      &copy; Copyright <strong><span>2022-2023</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+      
+      Designed by <a href="https://atulpratapsingh.netlify.app/">Atul Pratap Singh</a>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{url('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+  <script src="{{url('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{url('assets/vendor/chart.js/chart.umd.js')}}"></script>
+  <script src="{{url('assets/vendor/echarts/echarts.min.js')}}"></script>
+  <script src="{{url('assets/vendor/quill/quill.min.js')}}"></script>
+  <script src="{{url('assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
+  <script src="{{url('assets/vendor/tinymce/tinymce.min.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{url('assets/js/main.js')}}"></script>
+
+</body>
+
+</html>
