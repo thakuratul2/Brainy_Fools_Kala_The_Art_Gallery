96ee95b2da8fc4f042160ad42da4cc2402cb964b
thakuratul2 <pratapsinghatul111@gmail.com>
2023-06-03 11:48:27 +0530

    FIX

--- Modules/AdminUI/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('adminui::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('adminui.name') !!}
-    </p>
-@endsection
