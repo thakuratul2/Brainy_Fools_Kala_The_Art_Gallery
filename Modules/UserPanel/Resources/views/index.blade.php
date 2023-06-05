c5398c21778fe7e73de199f639270000424af53a
thakuratul2 <pratapsinghatul111@gmail.com>
2023-06-05 12:48:59 +0530

    FIX

--- Modules/UserPanel/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('userpanel::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('userpanel.name') !!}
-    </p>
-@endsection
