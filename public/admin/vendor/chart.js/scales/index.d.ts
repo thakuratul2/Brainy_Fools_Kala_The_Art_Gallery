54bc8bef16b6a65d7cf475b3c61963cce6f331f9
thakuratul2 <pratapsinghatul111@gmail.com>
2023-06-06 12:38:54 +0530

    FIX

--- public/admin/vendor/chart.js/scales/index.d.ts
+++ public/admin/vendor/chart.js/scales/index.d.ts
@@ -4,4 +4,3 @@ export { default as LogarithmicScale } from "./scale.logarithmic.js";
 export { default as RadialLinearScale } from "./scale.radialLinear.js";
 export { default as TimeScale } from "./scale.time.js";
 export { default as TimeSeriesScale } from "./scale.timeseries.js";
-g
