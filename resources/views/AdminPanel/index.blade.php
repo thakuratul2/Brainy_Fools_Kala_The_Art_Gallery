4ada3292cc387867b0b15f4e09aa35441c72d92f
thakuratul2 <pratapsinghatul111@gmail.com>
2023-06-11 09:13:56 +0530

    FIX

--- resources/views/AdminPanel/index.blade.php
+++ resources/views/AdminPanel/index.blade.php
@@ -238,7 +238,8 @@
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
-      </li><!-- End Dashboard Nav -->
+      </li>
+
 
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
