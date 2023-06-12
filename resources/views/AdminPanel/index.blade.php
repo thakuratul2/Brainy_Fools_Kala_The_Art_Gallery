232976832029c704b9abf3bf95634ffe21cffdf1
thakuratul2 <pratapsinghatul111@gmail.com>
2023-06-12 08:53:44 +0530

    FIX

--- resources/views/AdminPanel/index.blade.php
+++ resources/views/AdminPanel/index.blade.php
@@ -242,6 +242,7 @@
 
 
       <li class="nav-item">
+      
         <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-person"></i><span>Admin</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
