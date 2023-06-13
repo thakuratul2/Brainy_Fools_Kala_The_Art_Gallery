fd0d3f5c9914c0340e9c44236eb3e349eb30406a
thakuratul2 <pratapsinghatul111@gmail.com>
2023-06-13 08:43:34 +0530

    FIX

--- resources/views/AdminPanel/index.blade.php
+++ resources/views/AdminPanel/index.blade.php
@@ -246,6 +246,7 @@
         <a class="nav-link collapsed" data-bs-target="#components-nav" data-bs-toggle="collapse" href="#">
           <i class="bi bi-person"></i><span>Admin</span><i class="bi bi-chevron-down ms-auto"></i>
         </a>
+        
         <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
             <a href="{{url('/add-admin')}}">
